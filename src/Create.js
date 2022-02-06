dc47951145309a070e1ffae3afb71632a51f823a
Nadia-Hamid <com.nadiahamid>
2022-02-06 18:22:36 +0100

    Post request

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState("Write here");
   const [body, setBody] = useState("Write here");
   const [author, setAuthor] = useState("maria");
+  const [ispending, setPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    console.log(blog);
+    setPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setPending(false)
+    });
   };
 
   return (
@@ -30,13 +40,12 @@ const Create = () => {
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select 
-            value={author} 
-            onChange={(e) => setAuthor(e.target.value)}>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!ispending && <button>Add Blog</button>}
+        {ispending && <button disabled>Add Blog...</button>}
       </form>
     </div>
   );
