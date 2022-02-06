2f3338758be2da9c981593887388d8c6c30d069a
Nadia-Hamid <com.nadiahamid>
2022-02-06 18:10:49 +0100

    Submit Events

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,17 @@ const Create = () => {
   const [body, setBody] = useState("Write here");
   const [author, setAuthor] = useState("maria");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  };
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -24,15 +31,12 @@ const Create = () => {
         ></textarea>
         <label>Blog author:</label>
         <select 
-        value={author} 
-        onChange={(e) => setAuthor(e.target.value)}>
+            value={author} 
+            onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
         <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
       </form>
     </div>
   );
